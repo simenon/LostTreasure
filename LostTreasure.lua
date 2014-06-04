61bd4d4d86a55ccd1394c200e89b91710a033bf1
simenon <albert@simenon.nl>
2014-06-04 23:08:18 +0200

    Official bump to 1.0

---
+++ LostTreasure.lua
@@ -0,0 +1,138 @@
+local LAM = LibStub:GetLibrary("LibAddonMenu-1.0")
+local LOST_TREASURE = ZO_Object:Subclass()
+
+local AddonName = "LostTreasure" 
+local Author = "CrazyDutchGuy"
+
+
+LT =
+{	
+	defaults = 
+	{
+    	showTreasure = true,	
+		showtreasureswithoutmap = false,
+	}
+}
+
+local pinLayout_Treasure = 
+{ 
+		level = 40,		
+		texture = "LostTreasure/Icons/map.dds",
+		size = 32,	
+}
+
+--Handles info on TreasureMap
+local function GetInfoFromTag(pin)
+	local _, pinTag = pin:GetPinTypeAndTag()
+	
+	return pinTag[LOST_TREASURE_INDEX.MAP_NAME] 
+end
+
+--Creates ToolTip from treasure info
+local pinTooltipCreator = {
+	creator = function(pin)				
+		InformationTooltip:AddLine(GetInfoFromTag(pin), "", ZO_HIGHLIGHT_TEXT:UnpackRGB())--name color
+	end,
+	tooltip = InformationTooltip,
+}
+	
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+local function hasMap(mapName)  	
+    for i=1,GetNumViewableTreasureMaps() do
+        local map, _ = GetTreasureMapInfo(i)        
+        if map == mapName then 
+            return true
+        end
+    end
+    return false
+end
+
+local function pinCreator_Treasure(pinManager)
+    if not LT.SavedVariables.showTreasure then
+        return
+    end
+         
+    local data = LOST_TREASURE_DATA[GetCurrentMapZoneIndex()]
+    if GetMapType() == 1 then  --subzone in the current map, derive info from texture instead of mapname to avoid issues with french en german clients
+    	subzone = string.match(GetMapTileTexture(), "%w+/%w+/%w+/(%w+)_%w+_%d.dds")
+    	data = LOST_TREASURE_DATA[subzone]
+    end
+    
+    if not data then
+        return
+    end
+           
+    for _, pinData in pairs(data) do
+	    if LT.SavedVariables.showtreasureswithoutmap == true then
+            pinManager:CreatePin( _G[AddonName.."MapPin"], pinData, pinData[LOST_TREASURE_INDEX.X], pinData[LOST_TREASURE_INDEX.Y])
+		else
+		    if hasMap(pinData[LOST_TREASURE_INDEX.MAP_NAME]) then 		    	
+		        pinManager:CreatePin( _G[AddonName.."MapPin"], pinData, pinData[LOST_TREASURE_INDEX.X], pinData[LOST_TREASURE_INDEX.Y])
+            end
+		end	
+    end
+end
+	
+local function ShowTreasure()	
+	if LT.SavedVariables.showTreasure == true then  		
+		LT.MapPins:CreatePinType(AddonName.."MapPin", pinLayout_Treasure, pinTooltipCreator, pinCreator_Treasure)
+	else
+		LT.MapPins:enablePins(AddonName.."MapPin", disable )	     		 
+    end
+    LT.MapPins:RefreshPins()
+end
+
+local function GetTreasure()
+	return LT.SavedVariables.showTreasure
+end
+
+local function SetTreasure()		
+    LT.SavedVariables.showTreasure = not LT.SavedVariables.showTreasure	
+    ShowTreasure()
+end
+
+local function GetWithoutMap()
+	return LT.SavedVariables.showtreasureswithoutmap
+end
+
+local function SetWithoutMap()
+    LT.SavedVariables.showtreasureswithoutmap = not LT.SavedVariables.showtreasureswithoutmap
+    ShowTreasure()
+end
+
+function LOST_TREASURE:EVENT_SHOW_TREASURE_MAP(event, treasureMapIndex)
+	ShowTreasure()
+end
+
+
+function LOST_TREASURE:EVENT_ADD_ON_LOADED(event, name)
+   	if name == AddonName then 
+
+   		EVENT_MANAGER:RegisterForEvent(AddonName, EVENT_SHOW_TREASURE_MAP, function(...) LOST_TREASURE:EVENT_SHOW_TREASURE_MAP(...) end)	
+
+   		LT.SavedVariables = ZO_SavedVars:New("LostTreasure_SV", 1, nil, LT.defaults)
+
+		LT.MapPins = CustomMapPins:New() 
+
+		if GetWithoutMap() or GetTreasure() then
+			ShowTreasure()
+		end
+	
+		local addonMenu = LAM:CreateControlPanel(AddonName.."OptionsPanel", "Lost Treasure")
+		LAM:AddHeader(addonMenu, AddonName.."OptionsHeader", "Settings")
+		LAM:AddCheckbox(addonMenu, AddonName.."showTreasure", "Show Used Treasure Map", "Show/hide Used Treasure Map.", GetTreasure, SetTreasure)
+		LAM:AddCheckbox(addonMenu, AddonName.."showtreasureswithoutmap", "Show All Treasure Maps ", "Show/hide All Treasure Map Locations.", GetWithoutMap, SetWithoutMap)
+		LAM:AddHeader(addonMenu, AddonName.."DescriptionHeader", "Description")
+		LAM:AddDescription(addonMenu, AddonName.."_Description", "Icons will show on map only if the treasure map is used from your inventory.")
+		LAM:AddHeader(addonMenu, AddonName.."InfoHeader", "Info")
+		LAM:AddDescription(addonMenu, AddonName.."Author", "Author: " .. Author )
+
+		EVENT_MANAGER:UnregisterForEvent(AddonName, EVENT_ADD_ON_LOADED)
+
+	end
+end
+
+function TreasureMap_OnInitialized()	
+	EVENT_MANAGER:RegisterForEvent(AddonName, EVENT_ADD_ON_LOADED, function(...) LOST_TREASURE:EVENT_ADD_ON_LOADED(...) end)
+end
---
+++ LostTreasureData.lua
@@ -0,0 +1,237 @@
+LOST_TREASURE_INDEX = {
+	X = 1,
+	Y = 2,
+	MAP_NAME = 3,
+}
+
+LOST_TREASURE_DATA = { 
+----Auridon
+	[178] = {
+	    {0.49, 0.88, "Auridon Treasure Map I"},
+	    {0.48, 0.63, "Auridon Treasure Map II"},
+	    {0.44, 0.50, "Auridon Treasure Map III"},
+	    {0.66, 0.41, "Auridon Treasure Map IV"},
+	    {0.50, 0.26, "Auridon Treasure Map V"},--checked
+	    {0.32, 0.11, "Auridon Treasure Map VI"},
+		{0.69, 0.96, "Auridon CE Treasure Map"},--checked
+	},
+----Bal Foyen
+	[110] = {	
+	    {0.58, 0.71, "Bal Foyen Treasure Map I"},
+	    {0.24, 0.53, "Bal Foyen Treasure Map II"},--checked
+		{0.52, 0.42, "Bal Foyen CE Treasure Map"},--checked
+	},	
+----Eastmarch
+	[15] = {
+	    {0.43, 0.36, "Eastmarch Treasure Map I"},
+	    {0.31, 0.45, "Eastmarch Treasure Map II"},
+	    {0.43, 0.58, "Eastmarch Treasure Map III"},
+	    {0.36, 0.58, "Eastmarch Treasure Map IV"},
+	    {0.72, 0.65, "Eastmarch Treasure Map V"},
+	    {0.60, 0.53, "Eastmarch Treasure Map VI"},
+		{0.71, 0.58, "Eastmarch CE Treasure Map"},--checked
+	},
+----Betnikh
+	[293] = {
+	    {0.16, 0.30, "Betnikh Treasure Map I"},
+	    {0.56, 0.87, "Betnikh Treasure Map II"},
+	    {0.45, 0.43, "Betnikh CE Treasure Map"},--checked
+	},	
+----Stormhaven
+	[4] = {	
+    	{0.21, 0.51, "Stormhaven Treasure Map I"},
+	    {0.24, 0.34, "Stormhaven Treasure Map II"},
+	    {0.39, 0.46, "Stormhaven Treasure Map III"},
+	    {0.41, 0.58, "Stormhaven Treasure Map IV"},
+	    {0.68, 0.49, "Stormhaven Treasure Map V"},
+	    {0.79, 0.51, "Stormhaven Treasure Map VI"},
+	    {0.47, 0.45, "Stormhaven CE Treasure Map"},--checked
+	},	
+----Rivenspire
+	[5] = {	
+	    {0.32, 0.50, "Rivenspire Treasure Map I"},
+	    {0.17, 0.61, "Rivenspire Treasure Map II"},
+	    {0.71, 0.43, "Rivenspire Treasure Map III"},
+	    {0.71, 0.70, "Rivenspire Treasure Map IV"},
+	    {0.75, 0.30, "Rivenspire Treasure Map V"},
+	    {0.40, 0.29, "Rivenspire Treasure Map VI"},
+	    {0.75, 0.16, "Rivenspire CE Treasure Map"},
+	},
+----Rivenspire, North Point	
+    ["northpoint"] = {	
+       {0.28, 0.18, "Rivenspire CE Treasure Map"},
+    },		
+----Alik’r Desert
+	[17] = {
+	    {0.35, 0.72, "Alik'r Treasure Map I"},
+	    {0.04, 0.52, "Alik'r Treasure Map II"},
+		{0.63, 0.63, "Alik'r Treasure Map III"},
+		{0.59, 0.21, "Alik'r Treasure Map IV"},
+		{0.82, 0.52, "Alik'r Treasure Map V"},
+		{0.74, 0.45, "Alik'r Treasure Map VI"},
+	    {0.29, 0.34, "Alik'r CE Treasure Map"},
+	},	
+----Bangkorai
+	[14] = {	
+    	{0.42, 0.26, "Bangkorai Treasure Map I"},
+	    {0.61, 0.20, "Bangkorai Treasure Map II"},
+	    {0.72, 0.38, "Bangkorai Treasure Map III"},--checked
+	    {0.31, 0.45, "Bangkorai Treasure Map IV"},
+	    {0.32, 0.68, "Bangkorai Treasure Map V"},
+	    {0.64, 0.69, "Bangkorai Treasure Map VI"},
+	    {0.61, 0.76, "Bangkorai CE Treasure Map"},--checked
+	},
+----Bleakrock Isle
+	[109] = {	
+	    {0.43, 0.40, "Bleakrock Treasure Map I"},
+	    {0.42, 0.20, "Bleakrock Treasure Map II"},
+		{0.46, 0.65, "Bleakrock CE Treasure Map"},--checked
+	},
+----Bleakrock Isle, Bleackrock Village	
+	["bleakrockvillage"] = {
+        {0.55, 0.67, "Bleakrock CE Treasure Map"},--checked
+    },
+----Deshaan
+    [10] = {	
+	    {0.25, 0.54, "Deshaan Treasure Map I"},
+	    {0.17, 0.46, "Deshaan Treasure Map II"},
+	    {0.45, 0.40, "Deshaan Treasure Map III"},
+	    {0.75, 0.55, "Deshaan Treasure Map IV"},
+	    {0.89, 0.54, "Deshaan Treasure Map V"},
+	    {0.78, 0.50, "Deshaan Treasure Map VI"},
+		{0.35, 0.64, "Deshaan CE Treasure Map"},--checked
+    },
+----Glenumbra
+	[2] = {	
+	    {0.52, 0.75, "Glenumbra Treasure Map I"},
+		{0.49, 0.45, "Glenumbra Treasure Map II"},--checked
+		{0.65, 0.25, "Glenumbra Treasure Map III"},
+		{0.72, 0.52, "Glenumbra Treasure Map IV"},
+		{0.27, 0.52, "Glenumbra Treasure Map V"},
+		{0.51, 0.24, "Glenumbra Treasure Map VI"},
+		{0.73, 0.46, "Glenumbra CE Treasure Map"},
+	},
+----Grahtwood
+	[180] = {
+	    {0.39, 0.66, "Grahtwood Treasure Map I"},
+	    {0.64, 0.47, "Grahtwood Treasure Map II"},
+		{0.63, 0.38, "Grahtwood Treasure Map III"},--checked
+	    {0.46, 0.33, "Grahtwood Treasure Map IV"},
+	    {0.35, 0.35, "Grahtwood Treasure Map V"},
+	    {0.45, 0.44, "Grahtwood Treasure Map VI"},
+		{0.31, 0.60, "Grahtwood CE Treasure Map"},--double checked
+ 	},	
+----Greenshade
+	[18] = {
+	    {0.65, 0.83, "Greenshade Treasure Map I"},
+	    {0.72, 0.73, "Greenshade Treasure Map II"},
+	    {0.35, 0.49, "Greenshade Treasure Map III"},
+	    {0.32, 0.32, "Greenshade Treasure Map IV"},
+	    {0.24, 0.14, "Greenshade Treasure Map V"},
+	    {0.59, 0.37, "Greenshade Treasure Map VI"},
+		{0.58, 0.80, "Greenshade CE Treasure Map"},
+	},	
+----Khenarthi's Roost
+	[294] = {
+	    {0.62, 0.75, "Khenarthi's Roost Treasure Map I"},
+		{0.21, 0.31, "Khenarthi's Roost Treasure Map II"},
+		{0.41, 0.58, "Khenarthi's Roost Treasure Map III"},--checked
+		{0.77, 0.33, "Khenarthi's Roost Treasure Map IV"},
+		{0.62, 0.83, "Khenarthi's Roost CE Treasure Map I"},--checked
+	    {0.40, 0.37, "Khenarthi's Roost CE Treasure Map II"},--checked
+	},	
+----Malabal Tor 
+	[11] = {
+	    {0.19, 0.49, "Malabal Tor Treasure Map I"},
+	    {0.04, 0.47, "Malabal Tor Treasure Map II"},
+	    {0.49, 0.67, "Malabal Tor Treasure Map III"},
+	    {0.65, 0.70, "Malabal Tor Treasure Map IV"},--checked
+	    {0.79, 0.27, "Malabal Tor Treasure Map V"},
+	    {0.66, 0.21, "Malabal Tor Treasure Map VI"},
+        {0.11, 0.51, "Malabal Tor CE Treasure Map"},
+	},	
+----Reaper’s March
+	[179] = {
+	    {0.37, 0.42, "Reaper's March Treasure Map I"},
+	    {0.32, 0.12, "Reaper's March Treasure Map II"},
+	    {0.26, 0.64, "Reaper's March Treasure Map III"},
+	    {0.43, 0.67, "Reaper's March Treasure Map IV"},
+	    {0.55, 0.42, "Reaper's March Treasure Map V"},--checked
+	    {0.66, 0.22, "Reaper's March Treasure Map VI"},
+		{0.40, 0.53, "Reaper's March CE Treasure Map"},
+	},				
+----Shadowfen 
+	[19] = {	
+	    {0.37, 0.15, "Shadowfen Treasure Map I"},--checked
+	    {0.71, 0.39, "Shadowfen Treasure Map II"},--checked
+	    {0.70, 0.69, "Shadowfen Treasure Map III"},
+	    {0.61, 0.61, "Shadowfen Treasure Map IV"},
+	    {0.40, 0.47, "Shadowfen Treasure Map V"},
+	    {0.23, 0.55, "Shadowfen Treasure Map VI"},
+		{0.64, 0.46, "Shadowfen CE Treasure Map"},--checked
+	},
+--Stonefalls
+	[9] = {	
+	    {0.78, 0.56, "Stonefalls Treasure Map I"},
+	    {0.62, 0.51, "Stonefalls Treasure Map II"},--checked
+	    {0.47, 0.58, "Stonefalls Treasure Map III"},
+	    {0.17, 0.44, "Stonefalls Treasure Map IV"},
+	    {0.19, 0.54, "Stonefalls Treasure Map V"},
+	    {0.36, 0.70, "Stonefalls Treasure Map VI"},--checked
+	    {0.52, 0.61, "Stonefalls CE Treasure Map"},--checked
+    },
+----Stros M'Kai
+    [292] = {
+	    {0.53, 0.63, "Stros M'Kai Treasure Map I"},
+	    {0.09, 0.87, "Stros M'Kai Treasure Map II"},
+		{0.70, 0.33, "Stros M'Kai CE Treasure Map"},--checked
+	},
+----The Rift
+	[16] = {	
+	    {0.73, 0.36, "The Rift Treasure Map I"},
+	    {0.46, 0.43, "The Rift Treasure Map II"},
+	    {0.22, 0.35, "The Rift Treasure Map III"},
+	    {0.46, 0.54, "The Rift Treasure Map IV"},
+	    {0.83, 0.56, "The Rift Treasure Map V"},
+	    {0.74, 0.63, "The Rift Treasure Map VI"},
+	    {0.58, 0.61, "The Rift CE Treasure Map"},--checked
+	},
+----Coldharbour
+	[154] = {	
+	    {0.39, 0.82, "Coldharbour Treasure Map I"},
+	    {0.39, 0.56, "Coldharbour Treasure Map II"},
+	    {0.59, 0.69, "Coldharbour Treasure Map III"},
+	    {0.74, 0.76, "Coldharbour Treasure Map IV"},
+	    {0.42, 0.44, "Coldharbour Treasure Map V"},
+	    {0.49, 0.38, "Coldharbour Treasure Map VI"},
+	    {0.54, 0.40, "Coldharbour CE Treasure Map"},
+	},	
+----Cyrodiil
+	[37] = {
+		{0.40, 0.48, "Cyrodiil Treasure Map I"},
+	    {0.51, 0.82, "Cyrodiil Treasure Map II"},
+	    {0.28, 0.70, "Cyrodiil Treasure Map III"},
+	    {0.64, 0.75, "Cyrodiil Treasure Map IV"},
+	    {0.48, 0.52, "Cyrodiil Treasure Map V"},
+	    {0.23, 0.55, "Cyrodiil Treasure Map VI"},
+	    {0.54, 0.13, "Cyrodiil Treasure Map VII"},
+		{0.17, 0.37, "Cyrodiil Treasure Map VIII"},
+		{0.31, 0.36, "Cyrodiil Treasure Map IX"},
+		{0.30, 0.17, "Cyrodiil Treasure Map X"},
+		{0.37, 0.14, "Cyrodiil Treasure Map XI"},
+		{0.47, 0.18, "Cyrodiil Treasure Map XII"},
+		{0.82, 0.27, "Cyrodiil Treasure Map XIII"},
+		{0.69, 0.51, "Cyrodiil Treasure Map XIV"},
+		{0.66, 0.46, "Cyrodiil Treasure Map XV"},
+		{0.67, 0.62, "Cyrodiil Treasure Map XVI"},
+		{0.76, 0.46, "Cyrodiil Treasure Map XVII"},
+		{0.74, 0.51, "Cyrodiil Treasure Map XVIII"},
+	},
+	----Craglorn
+	[352] = {	
+	    {0.21, 0.56, "Craglorn Treasure Map I"},
+	    {0.37, 0.38, "Craglorn Treasure Map II"},
+	    {0.70, 0.48, "Craglorn Treasure Map III"},
+	    {0.64, 0.61, "Craglorn Treasure Map IV"},
+	},	
+}
