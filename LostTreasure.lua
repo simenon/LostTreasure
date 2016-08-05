ad3a103da40f0cff35362b20d2466c7fe934fdfa
Albert Simenon <simena@localhost.localdomain>
2016-08-05 09:36:25 +0200

    Pushed version to 4.16 and bumped API.

--- LostTreasure.lua
+++ LostTreasure.lua
@@ -5,7 +5,7 @@ local Addon = {
     Name = "LostTreasure",
     NameSpaced = "Lost Treasure",
     Author = "CrazyDutchGuy ",
-    Version = "4.15",
+    Version = "4.16",
 }
 
 LT = ZO_Object:Subclass()
