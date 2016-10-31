ce69a7e6e1391d0fb8631e992df7f5ed3e06bde4
Albert Simenon <albert@simenon.nl>
2016-10-31 19:51:09 +0100

    Version V4.22

--- LostTreasure.lua
+++ LostTreasure.lua
@@ -5,7 +5,7 @@ local Addon = {
     Name = "LostTreasure",
     NameSpaced = "Lost Treasure",
     Author = "CrazyDutchGuy ",
-    Version = "4.21",
+    Version = "4.22",
 }
 
 LT = ZO_Object:Subclass()
