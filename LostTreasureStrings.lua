543a797d37dc92092c33e772b9841d2c1a0aa8b2
Albert Simenon <albert@simenon.nl>
2017-07-14 12:54:44 +0200

    Added french translation for menu options

--- LostTreasureStrings.lua
+++ LostTreasureStrings.lua
@@ -1,177 +1,177 @@
 LOST_TREASURE_STRINGS = {
-	["en"] = {
-		TREASURE_ON_MAP = "Show Treasure on Map",
-		TREASURE_ON_MAP_TOOLTIP = "Show treasure map locations on player map?",
-		TREASURE_ON_COMPASS = "Show Treasure on Compass",
-		TREASURE_ON_COMPASS_TOOLTIP = "Show treasure map locations on compass bar?",
-		TREASURE_ICON = "Treasure Pin Texture",
-		TREASURE_ICON_TOOLTIP = "Which pin texture to use for treasure map locations",
-		TREASURE_MARK_WHICH = "Treasure Map Marking",
-		TREASURE_MARK_WHICH_TOOLTIP = "When to mark treasure locations on the player map",
-		
-		SURVEYS_ON_MAP = "Show Crafting Surveys on Map",
-		SURVEYS_ON_MAP_TOOLTIP = "Show crafting survey locations on player map?",
-		SURVEYS_ON_COMPASS = "Show Crafting Surveys on Compass",
-		SURVEYS_ON_COMPASS_TOOLTIP = "Show crafting survey locations on compass bar?",
-		SURVEYS_ICON = "Survey Pin Texture",
-		SURVEYS_ICON_TOOLTIP = "Which pin texture to use for crafting survey locations",
-		SURVEYS_MARK_WHICH = "Survey Map Marking",
-		SURVEYS_MARK_WHICH_TOOLTIP = "When to mark survey locations on the player map",
-		
-		PIN_SIZE = "Pin Texture Size",
-		PIN_SIZE_TOOLTIP = "Size of the pin",
+  ["en"] = {
+    TREASURE_ON_MAP = "Show Treasure on Map",
+    TREASURE_ON_MAP_TOOLTIP = "Show treasure map locations on player map?",
+    TREASURE_ON_COMPASS = "Show Treasure on Compass",
+    TREASURE_ON_COMPASS_TOOLTIP = "Show treasure map locations on compass bar?",
+    TREASURE_ICON = "Treasure Pin Texture",
+    TREASURE_ICON_TOOLTIP = "Which pin texture to use for treasure map locations",
+    TREASURE_MARK_WHICH = "Treasure Map Marking",
+    TREASURE_MARK_WHICH_TOOLTIP = "When to mark treasure locations on the player map",
+    
+    SURVEYS_ON_MAP = "Show Crafting Surveys on Map",
+    SURVEYS_ON_MAP_TOOLTIP = "Show crafting survey locations on player map?",
+    SURVEYS_ON_COMPASS = "Show Crafting Surveys on Compass",
+    SURVEYS_ON_COMPASS_TOOLTIP = "Show crafting survey locations on compass bar?",
+    SURVEYS_ICON = "Survey Pin Texture",
+    SURVEYS_ICON_TOOLTIP = "Which pin texture to use for crafting survey locations",
+    SURVEYS_MARK_WHICH = "Survey Map Marking",
+    SURVEYS_MARK_WHICH_TOOLTIP = "When to mark survey locations on the player map",
+    
+    PIN_SIZE = "Pin Texture Size",
+    PIN_SIZE_TOOLTIP = "Size of the pin",
+    
+    PIN_LEVEL = "Pin Map Level",
+    PIN_LEVEL_TOOLTIP = "Higher level draws pin over lower level pins. Increase if pin is obscured by other pins.",
+    
+    SHOW_MINIMAP = "Show Mini Treasure Map",
+    SHOW_MINIMAP_TOOLTIP = "Show the mini treasure map on screen when using map from inventory?",
+    
+    MARK_MAP_MENU_OPTION1 = "Mark on use",
+    MARK_MAP_MENU_OPTION2 = "Mark all in inventory",
+    MARK_MAP_MENU_OPTION3 = "Mark all locations",
+    
+    MARKER_DELAY = "BETA : Marker Deletion Delay",
+    MARKER_DELAY_TOOLTIP = "Adds a delay before deleting a marked location on the map. Only works in conjunction with Mark on Use or Mark all in Inventory. This will not work when you open up the map as it refreshes all pins then.",
+  },
+  ["fr"] = {
+    TREASURE_ON_MAP = "Afficher les trésors sur la carte",
+    TREASURE_ON_MAP_TOOLTIP = "Affiche la position des trésors sur la carte du monde.",
+    TREASURE_ON_COMPASS = "Afficher les trésors dans la boussole",
+    TREASURE_ON_COMPASS_TOOLTIP = "Affiche la position des trésors dans la boussole.",
+    TREASURE_ICON = "Apparence des marqueurs de trésor",
+    TREASURE_ICON_TOOLTIP = "Détermine l'apparence des marqueurs de trésors.",
+    TREASURE_MARK_WHICH = "Affichage des marqueurs de trésor",
+    TREASURE_MARK_WHICH_TOOLTIP = "Détermine si tous les marqueurs de trésors sont affichés sur la carte ou seuls ceux dont la carte est possédé.",
+    
+    SURVEYS_ON_MAP = "Afficher les repérages d'artisanat sur la carte",
+    SURVEYS_ON_MAP_TOOLTIP = "Affiche la position des repérages d'artisanat sur la carte du monde.",
+    SURVEYS_ON_COMPASS = "Afficher les repérages d'artisanat dans la boussole",
+    SURVEYS_ON_COMPASS_TOOLTIP = "Affiche la position des repérages d'artisanat dans la boussole.",
+    SURVEYS_ICON = "Apparence des marqueurs de repérage",
+    SURVEYS_ICON_TOOLTIP = "Détermine l'apparence des marqueurs de repérage d'artisanat.",
+    SURVEYS_MARK_WHICH = "Affichage des marqueurs de repérage",
+    SURVEYS_MARK_WHICH_TOOLTIP = "Détermine si tous les marqueurs de repérage d'artisanat sont affichés sur la carte ou seuls ceux dont le repérage est possédé.",
+    
+    PIN_SIZE = "Taille des marqueurs",
+    PIN_SIZE_TOOLTIP = "Détermine la taille des marqueurs.",
 
-		PIN_LEVEL = "Pin Map Level",
-		PIN_LEVEL_TOOLTIP = "Higher level draws pin over lower level pins. Increase if pin is obscured by other pins.",
-		
-		SHOW_MINIMAP = "Show Mini Treasure Map",
-		SHOW_MINIMAP_TOOLTIP = "Show the mini treasure map on screen when using map from inventory?",
+    PIN_LEVEL = "Niveau des marqueurs",
+    PIN_LEVEL_TOOLTIP = "Un niveau plus élevé positionne le marqueur au-dessus des autres. Augmenter cette valeur si le marqueur est caché par un autre.",
+    
+    SHOW_MINIMAP = "Afficher la mini-carte au trésor",
+    SHOW_MINIMAP_TOOLTIP = "Affiche une mini-carte lorsque une carte est utilisée depuis l'inventaire.",
 
-		MARK_MAP_MENU_OPTION1 = "Mark on use",
-		MARK_MAP_MENU_OPTION2 = "Mark all in inventory",
-		MARK_MAP_MENU_OPTION3 = "Mark all locations",
-		
-		MARKER_DELAY = "BETA : Marker Deletion Delay",
-		MARKER_DELAY_TOOLTIP = "Adds a delay before deleting a marked location on the map. Only works in conjunction with Mark on Use or Mark all in Inventory. This will not work when you open up the map as it refreshes all pins then.",
-	},
-	["fr"] = {
-		TREASURE_ON_MAP = "Show Treasure on Map",
-		TREASURE_ON_MAP_TOOLTIP = "Show treasure map locations on player map?",
-		TREASURE_ON_COMPASS = "Show Treasure on Compass",
-		TREASURE_ON_COMPASS_TOOLTIP = "Show treasure map locations on compass bar?",
-		TREASURE_ICON = "Treasure Pin Texture",
-		TREASURE_ICON_TOOLTIP = "Which pin texture to use for treasure map locations",
-		TREASURE_MARK_WHICH = "Treasure Map Marking",
-		TREASURE_MARK_WHICH_TOOLTIP = "When to mark treasure locations on the player map",
-		
-		SURVEYS_ON_MAP = "Show Crafting Surveys on Map",
-		SURVEYS_ON_MAP_TOOLTIP = "Show crafting survey locations on player map?",
-		SURVEYS_ON_COMPASS = "Show Crafting Surveys on Compass",
-		SURVEYS_ON_COMPASS_TOOLTIP = "Show crafting survey locations on compass bar?",
-		SURVEYS_ICON = "Survey Pin Texture",
-		SURVEYS_ICON_TOOLTIP = "Which pin texture to use for crafting survey locations",
-		SURVEYS_MARK_WHICH = "Survey Map Marking",
-		SURVEYS_MARK_WHICH_TOOLTIP = "When to mark survey locations on the player map",
-		
-		PIN_SIZE = "Pin Texture Size",
-		PIN_SIZE_TOOLTIP = "Size of the pin",
-
-		PIN_LEVEL = "Pin Map Level",
-		PIN_LEVEL_TOOLTIP = "Higher level draws pin over lower level pins. Increase if pin is obscured by other pins.",
-		
-		SHOW_MINIMAP = "Show Mini Treasure Map",
-		SHOW_MINIMAP_TOOLTIP = "Show the mini treasure map on screen when using map from inventory?",
-
-		MARK_MAP_MENU_OPTION1 = "Mark on use",
-		MARK_MAP_MENU_OPTION2 = "Mark all in inventory",
-		MARK_MAP_MENU_OPTION3 = "Mark all locations",
-		
-		MARKER_DELAY = "BETA : Marker Deletion Delay",
-		MARKER_DELAY_TOOLTIP = "Adds a delay before deleting a marked location on the map. Only works in conjunction with Mark on Use or Mark all in Inventory. This will not work when you open up the map as it refreshes all pins then.",
-	},
-	["de"] = {
-		TREASURE_ON_MAP = "Schatzkarten auf Karte",
-		TREASURE_ON_MAP_TOOLTIP = "Zeigt die Position der Schatzkarten auf der Karte an.",
-		TREASURE_ON_COMPASS = "Schatzkarten in Kompass",
-		TREASURE_ON_COMPASS_TOOLTIP = "Zeigt die Position der Schatzkarten im Kompass an.",
-		TREASURE_ICON = "Schatzkarten Symbol",
-		TREASURE_ICON_TOOLTIP = "Wähle das gewünschte Symbol für die Schatzkarten.",
-		TREASURE_MARK_WHICH = "Schatzkarten Markierung",
-		TREASURE_MARK_WHICH_TOOLTIP = "Wann sollen die Positionen der Schatzkarte auf der Karte angezeigt werden?",
-		
-		SURVEYS_ON_MAP = "Handwerksgutachten auf Karte",
-		SURVEYS_ON_MAP_TOOLTIP = "Zeigt die Position der Handwerksgutachten auf der Karte an.",
-		SURVEYS_ON_COMPASS = "Handwerksgutachten im Kompass",
-		SURVEYS_ON_COMPASS_TOOLTIP = "Zeigt die Position der Handwerksgutachten im Kompass an.",
-		SURVEYS_ICON = "Handwerksgutachten Symbol",
-		SURVEYS_ICON_TOOLTIP = "Wähle das gewünschte Symbol für die Handwerksgutachten.",
-		SURVEYS_MARK_WHICH = "Handwerksgutachten Markierung",
-		SURVEYS_MARK_WHICH_TOOLTIP = "Wann sollen die Positionen der Handwerksgutachten auf der Karte angezeigt werden?",
-		
-		PIN_SIZE = "Symbolgrösse",
-		PIN_SIZE_TOOLTIP = "Grösse der Symbole auf der Karte",
-
-		PIN_LEVEL = "Pin Map Level",
-		PIN_LEVEL_TOOLTIP = "Higher level draws pin over lower level pins. Increase if pin is obscured by other pins.",
-		
-		SHOW_MINIMAP = "Zeige kleine Schatzkarte",
-		SHOW_MINIMAP_TOOLTIP = "Zeigt eine kleine Schatzkarte auf deinem Bildschirm, wenn du eine Schatzkarte aus deinem Inventar öffnest.",
-		
-		MARK_MAP_MENU_OPTION1 = "Markiere bei Benutzung",
-		MARK_MAP_MENU_OPTION2 = "Markiere alle im Inventar",
-		MARK_MAP_MENU_OPTION3 = "Markiere alle Standorte",
-		
-		MARKER_DELAY = "BETA: Verzögerung zur Löschung der Markierung",
-		MARKER_DELAY_TOOLTIP = "Hier kannst du Verzögerung in Sekunden einstellen, bevor eine Markierung gelöscht wird. Diese Einstellung funktioniert nur, wenn du oben \'Markiere alle im Inventar\' ausgewählt hast.",
-	},
-	["ru"] = {
-		TREASURE_ON_MAP = "Показывать сокровища на карте",
-		TREASURE_ON_MAP_TOOLTIP = "Показывать месторасположение карты сокровищ на карте игрока?",
-		TREASURE_ON_COMPASS = "Показывать сокровища на компасе",
-		TREASURE_ON_COMPASS_TOOLTIP = "Показывать месторасположение карты сокровищ на компасе?",
-		TREASURE_ICON = "Иконка маркера",
-		TREASURE_ICON_TOOLTIP = "Какой маркер использовать для месторасположения карты сокровищ",
-		TREASURE_MARK_WHICH = "Отмечать карты сокровищ",
-		TREASURE_MARK_WHICH_TOOLTIP = "Когда отмечать месторасположение карт сокровищ на карте игрока",
-		
-		SURVEYS_ON_MAP = "Показывать ремесл. исслед. на карте",
-		SURVEYS_ON_MAP_TOOLTIP = "Показывать ремесленные исследования на карте игрока?",
-		SURVEYS_ON_COMPASS = "Показывать ремесл. исслед. на компасе",
-		SURVEYS_ON_COMPASS_TOOLTIP = "Показывать ремесленные исследования на компасе?",
-		SURVEYS_ICON = "Иконка маркера исследований",
-		SURVEYS_ICON_TOOLTIP = "Какой маркер использовать для месторасположения исследований",
-		SURVEYS_MARK_WHICH = "Отмечать исследования",
-		SURVEYS_MARK_WHICH_TOOLTIP = "Когда отмечать месторасположение исследований на карте игрока",
-		
-		PIN_SIZE = "Размер иконки маркера",
-		PIN_SIZE_TOOLTIP = "Размер маркера",
-
-		PIN_LEVEL = "Уровень маркера",
-		PIN_LEVEL_TOOLTIP = "Увеличте, если маркер закрыт другими буквами.",
-		
-		SHOW_MINIMAP = "Показывать миниатюрную карту",
-		SHOW_MINIMAP_TOOLTIP = "Показывать миниатюрную карту сокровищ при использования карты через инвентарь?",
-
-		MARK_MAP_MENU_OPTION1 = "Отмечать по применению",
-		MARK_MAP_MENU_OPTION2 = "Отмечать все в инвентаре",
-		MARK_MAP_MENU_OPTION3 = "Отмечать все расположения",
-		
-		MARKER_DELAY = "BETA: Задержка удаления маркера",
-		MARKER_DELAY_TOOLTIP = "Добавляет задержку перед удалением помеченных расположений на карте. Работает только вместе с «Отметить по применению» или «Отметить все в инвентаре». Это не работает, когда вы открываете карту, так как в этом случае обновляются маркеры.",
-	},
-	["jp"] = {
-		TREASURE_ON_MAP = "マップに宝を表示",
-		TREASURE_ON_MAP_TOOLTIP = "プレイヤーマップに宝の位置を表示しますか？",
-		TREASURE_ON_COMPASS = "コンパスに宝を表示",
-		TREASURE_ON_COMPASS_TOOLTIP = "コンパスバーに宝の位置を表示しますか？",
-		TREASURE_ICON = "宝ピンテクスチャ",
-		TREASURE_ICON_TOOLTIP = "宝の地図の位置表示にどのピンテクスチャを使用するか選択します",
-		TREASURE_MARK_WHICH = "宝の地図マーキング",
-		TREASURE_MARK_WHICH_TOOLTIP = "プレイヤーマップにいつ宝をマークするかを選択します",
-		
-		SURVEYS_ON_MAP = "マップに原料の宝の地図を表示",
-		SURVEYS_ON_MAP_TOOLTIP = "プレイヤーマップに原料の宝の位置を表示しますか？",
-		SURVEYS_ON_COMPASS = "コンパスに原料の宝の地図を表示",
-		SURVEYS_ON_COMPASS_TOOLTIP = "コンパスバーに原料の宝の位置を表示しますか？",
-		SURVEYS_ICON = "原料の宝の地図のピンテクスチャ",
-		SURVEYS_ICON_TOOLTIP = "原料の宝の地図の位置表示にどのピンテクスチャを使用するか選択します",
-		SURVEYS_MARK_WHICH = "原料の宝の地図マーキング",
-		SURVEYS_MARK_WHICH_TOOLTIP = "プレイヤーマップにいつ原料の宝をマークするかを選択します",
-		
-		PIN_SIZE = "ピンテクスチャ―サイズ",
-		PIN_SIZE_TOOLTIP = "ピンのサイズを指定します",
-
-		PIN_LEVEL = "ピンマップレベル",
-		PIN_LEVEL_TOOLTIP = "レベルが大きいピンが小さいピンの上に描画されます。他のピンに隠れる場合は大きくします。",
-		
-		SHOW_MINIMAP = "ミニ宝の地図を表示",
-		SHOW_MINIMAP_TOOLTIP = "インベントリからマップを使用した際ミニ宝の地図を画面に表示しますか？",
-
-		MARK_MAP_MENU_OPTION1 = "使用中をマーク",
-		MARK_MAP_MENU_OPTION2 = "インベントリ内の全てをマーク",
-		MARK_MAP_MENU_OPTION3 = "全ての位置をマーク",
-		
-		MARKER_DELAY = "BETA : マーカー削除ディレイ",
-		MARKER_DELAY_TOOLTIP = "マップからマークされた位置を削除する前にディレイを追加します。使用中をマークかインベントリ内の全てをマークを使用中のみ動作します。マップを開くと全てのピンを更新してしまうため、機能しません。",
-	},
+    MARK_MAP_MENU_OPTION1 = "Utilisé",
+    MARK_MAP_MENU_OPTION2 = "Dans l'inventaire",
+    MARK_MAP_MENU_OPTION3 = "Tous",
+    
+    MARKER_DELAY = "BETA : Délai d'effacement des marqueurs",
+    MARKER_DELAY_TOOLTIP = "Ajoute un délai avant d'effacer un emplacement marqué sur la carte. Fonctionne seulement en conjonction avec 'Utilisé' et 'Dans l'inventaire'. Cela ne fonctionne pas lors de l'ouverture de la carte car tous les marqueurs sont actualisés.",
+  },
+  ["de"] = {
+    TREASURE_ON_MAP = "Schatzkarten auf Karte",
+    TREASURE_ON_MAP_TOOLTIP = "Zeigt die Position der Schatzkarten auf der Karte an.",
+    TREASURE_ON_COMPASS = "Schatzkarten in Kompass",
+    TREASURE_ON_COMPASS_TOOLTIP = "Zeigt die Position der Schatzkarten im Kompass an.",
+    TREASURE_ICON = "Schatzkarten Symbol",
+    TREASURE_ICON_TOOLTIP = "Wähle das gewünschte Symbol für die Schatzkarten.",
+    TREASURE_MARK_WHICH = "Schatzkarten Markierung",
+    TREASURE_MARK_WHICH_TOOLTIP = "Wann sollen die Positionen der Schatzkarte auf der Karte angezeigt werden?",
+    
+    SURVEYS_ON_MAP = "Handwerksgutachten auf Karte",
+    SURVEYS_ON_MAP_TOOLTIP = "Zeigt die Position der Handwerksgutachten auf der Karte an.",
+    SURVEYS_ON_COMPASS = "Handwerksgutachten im Kompass",
+    SURVEYS_ON_COMPASS_TOOLTIP = "Zeigt die Position der Handwerksgutachten im Kompass an.",
+    SURVEYS_ICON = "Handwerksgutachten Symbol",
+    SURVEYS_ICON_TOOLTIP = "Wähle das gewünschte Symbol für die Handwerksgutachten.",
+    SURVEYS_MARK_WHICH = "Handwerksgutachten Markierung",
+    SURVEYS_MARK_WHICH_TOOLTIP = "Wann sollen die Positionen der Handwerksgutachten auf der Karte angezeigt werden?",
+    
+    PIN_SIZE = "Symbolgrösse",
+    PIN_SIZE_TOOLTIP = "Grösse der Symbole auf der Karte",
+    
+    PIN_LEVEL = "Pin Map Level",
+    PIN_LEVEL_TOOLTIP = "Higher level draws pin over lower level pins. Increase if pin is obscured by other pins.",
+    
+    SHOW_MINIMAP = "Zeige kleine Schatzkarte",
+    SHOW_MINIMAP_TOOLTIP = "Zeigt eine kleine Schatzkarte auf deinem Bildschirm, wenn du eine Schatzkarte aus deinem Inventar öffnest.",
+    
+    MARK_MAP_MENU_OPTION1 = "Markiere bei Benutzung",
+    MARK_MAP_MENU_OPTION2 = "Markiere alle im Inventar",
+    MARK_MAP_MENU_OPTION3 = "Markiere alle Standorte",
+    
+    MARKER_DELAY = "BETA: Verzögerung zur Löschung der Markierung",
+    MARKER_DELAY_TOOLTIP = "Hier kannst du Verzögerung in Sekunden einstellen, bevor eine Markierung gelöscht wird. Diese Einstellung funktioniert nur, wenn du oben \'Markiere alle im Inventar\' ausgewählt hast.",
+  },
+  ["ru"] = {
+    TREASURE_ON_MAP = "Показывать сокровища на карте",
+    TREASURE_ON_MAP_TOOLTIP = "Показывать месторасположение карты сокровищ на карте игрока?",
+    TREASURE_ON_COMPASS = "Показывать сокровища на компасе",
+    TREASURE_ON_COMPASS_TOOLTIP = "Показывать месторасположение карты сокровищ на компасе?",
+    TREASURE_ICON = "Иконка маркера",
+    TREASURE_ICON_TOOLTIP = "Какой маркер использовать для месторасположения карты сокровищ",
+    TREASURE_MARK_WHICH = "Отмечать карты сокровищ",
+    TREASURE_MARK_WHICH_TOOLTIP = "Когда отмечать месторасположение карт сокровищ на карте игрока",
+    
+    SURVEYS_ON_MAP = "Показывать ремесл. исслед. на карте",
+    SURVEYS_ON_MAP_TOOLTIP = "Показывать ремесленные исследования на карте игрока?",
+    SURVEYS_ON_COMPASS = "Показывать ремесл. исслед. на компасе",
+    SURVEYS_ON_COMPASS_TOOLTIP = "Показывать ремесленные исследования на компасе?",
+    SURVEYS_ICON = "Иконка маркера исследований",
+    SURVEYS_ICON_TOOLTIP = "Какой маркер использовать для месторасположения исследований",
+    SURVEYS_MARK_WHICH = "Отмечать исследования",
+    SURVEYS_MARK_WHICH_TOOLTIP = "Когда отмечать месторасположение исследований на карте игрока",
+    
+    PIN_SIZE = "Размер иконки маркера",
+    PIN_SIZE_TOOLTIP = "Размер маркера",
+    
+    PIN_LEVEL = "Уровень маркера",
+    PIN_LEVEL_TOOLTIP = "Увеличте, если маркер закрыт другими буквами.",
+    
+    SHOW_MINIMAP = "Показывать миниатюрную карту",
+    SHOW_MINIMAP_TOOLTIP = "Показывать миниатюрную карту сокровищ при использования карты через инвентарь?",
+    
+    MARK_MAP_MENU_OPTION1 = "Отмечать по применению",
+    MARK_MAP_MENU_OPTION2 = "Отмечать все в инвентаре",
+    MARK_MAP_MENU_OPTION3 = "Отмечать все расположения",
+    
+    MARKER_DELAY = "BETA: Задержка удаления маркера",
+    MARKER_DELAY_TOOLTIP = "Добавляет задержку перед удалением помеченных расположений на карте. Работает только вместе с «Отметить по применению» или «Отметить все в инвентаре». Это не работает, когда вы открываете карту, так как в этом случае обновляются маркеры.",
+  },
+  ["jp"] = {
+    TREASURE_ON_MAP = "マップに宝を表示",
+    TREASURE_ON_MAP_TOOLTIP = "プレイヤーマップに宝の位置を表示しますか？",
+    TREASURE_ON_COMPASS = "コンパスに宝を表示",
+    TREASURE_ON_COMPASS_TOOLTIP = "コンパスバーに宝の位置を表示しますか？",
+    TREASURE_ICON = "宝ピンテクスチャ",
+    TREASURE_ICON_TOOLTIP = "宝の地図の位置表示にどのピンテクスチャを使用するか選択します",
+    TREASURE_MARK_WHICH = "宝の地図マーキング",
+    TREASURE_MARK_WHICH_TOOLTIP = "プレイヤーマップにいつ宝をマークするかを選択します",
+    
+    SURVEYS_ON_MAP = "マップに原料の宝の地図を表示",
+    SURVEYS_ON_MAP_TOOLTIP = "プレイヤーマップに原料の宝の位置を表示しますか？",
+    SURVEYS_ON_COMPASS = "コンパスに原料の宝の地図を表示",
+    SURVEYS_ON_COMPASS_TOOLTIP = "コンパスバーに原料の宝の位置を表示しますか？",
+    SURVEYS_ICON = "原料の宝の地図のピンテクスチャ",
+    SURVEYS_ICON_TOOLTIP = "原料の宝の地図の位置表示にどのピンテクスチャを使用するか選択します",
+    SURVEYS_MARK_WHICH = "原料の宝の地図マーキング",
+    SURVEYS_MARK_WHICH_TOOLTIP = "プレイヤーマップにいつ原料の宝をマークするかを選択します",
+    
+    PIN_SIZE = "ピンテクスチャ―サイズ",
+    PIN_SIZE_TOOLTIP = "ピンのサイズを指定します",
+    
+    PIN_LEVEL = "ピンマップレベル",
+    PIN_LEVEL_TOOLTIP = "レベルが大きいピンが小さいピンの上に描画されます。他のピンに隠れる場合は大きくします。",
+    
+    SHOW_MINIMAP = "ミニ宝の地図を表示",
+    SHOW_MINIMAP_TOOLTIP = "インベントリからマップを使用した際ミニ宝の地図を画面に表示しますか？",
+    
+    MARK_MAP_MENU_OPTION1 = "使用中をマーク",
+    MARK_MAP_MENU_OPTION2 = "インベントリ内の全てをマーク",
+    MARK_MAP_MENU_OPTION3 = "全ての位置をマーク",
+    
+    MARKER_DELAY = "BETA : マーカー削除ディレイ",
+    MARKER_DELAY_TOOLTIP = "マップからマークされた位置を削除する前にディレイを追加します。使用中をマークかインベントリ内の全てをマークを使用中のみ動作します。マップを開くと全てのピンを更新してしまうため、機能しません。",
+  },
 }
