ff305474c2200219f94c5bf80291aed8815f0371
Albert Simenon <albert@simenon.nl>
2018-05-30 22:12:17 +0200

    reworked localization support.

---
+++ LostTreasure/Lang/de.lua
@@ -0,0 +1,33 @@
+ZO_CreateStringId ("LTS_TREASURE_ON_MAP","Schatzkarten auf Karte")
+ZO_CreateStringId ("LTS_TREASURE_ON_MAP_TOOLTIP","Zeigt die Position der Schatzkarten auf der Karte an.")
+ZO_CreateStringId ("LTS_TREASURE_ON_COMPASS","Schatzkarten in Kompass")
+ZO_CreateStringId ("LTS_TREASURE_ON_COMPASS_TOOLTIP","Zeigt die Position der Schatzkarten im Kompass an.")
+ZO_CreateStringId ("LTS_TREASURE_ICON","Schatzkarten Symbol")
+ZO_CreateStringId ("LTS_TREASURE_ICON_TOOLTIP","Wähle das gewünschte Symbol für die Schatzkarten.")
+ZO_CreateStringId ("LTS_TREASURE_MARK_WHICH","Schatzkarten Markierung")
+ZO_CreateStringId ("LTS_TREASURE_MARK_WHICH_TOOLTIP","Wann sollen die Positionen der Schatzkarte auf der Karte angezeigt werden?")
+
+ZO_CreateStringId ("LTS_SURVEYS_ON_MAP","Handwerksgutachten auf Karte")
+ZO_CreateStringId ("LTS_SURVEYS_ON_MAP_TOOLTIP","Zeigt die Position der Handwerksgutachten auf der Karte an.")
+ZO_CreateStringId ("LTS_SURVEYS_ON_COMPASS","Handwerksgutachten im Kompass")
+ZO_CreateStringId ("LTS_SURVEYS_ON_COMPASS_TOOLTIP","Zeigt die Position der Handwerksgutachten im Kompass an.")
+ZO_CreateStringId ("LTS_SURVEYS_ICON","Handwerksgutachten Symbol")
+ZO_CreateStringId ("LTS_SURVEYS_ICON_TOOLTIP","Wähle das gewünschte Symbol für die Handwerksgutachten.")
+ZO_CreateStringId ("LTS_SURVEYS_MARK_WHICH","Handwerksgutachten Markierung")
+ZO_CreateStringId ("LTS_SURVEYS_MARK_WHICH_TOOLTIP","Wann sollen die Positionen der Handwerksgutachten auf der Karte angezeigt werden?")
+
+ZO_CreateStringId ("LTS_PIN_SIZE","Symbolgrösse")
+ZO_CreateStringId ("LTS_PIN_SIZE_TOOLTIP","Grösse der Symbole auf der Karte")
+
+ZO_CreateStringId ("LTS_PIN_LEVEL","Pin Map Level")
+ZO_CreateStringId ("LTS_PIN_LEVEL_TOOLTIP","Higher level draws pin over lower level pins. Increase if pin is obscured by other pins.")
+
+ZO_CreateStringId ("LTS_SHOW_MINIMAP","Zeige kleine Schatzkarte")
+ZO_CreateStringId ("LTS_SHOW_MINIMAP_TOOLTIP","Zeigt eine kleine Schatzkarte auf deinem Bildschirm, wenn du eine Schatzkarte aus deinem Inventar öffnest.")
+
+ZO_CreateStringId ("LTS_MARK_MAP_MENU_OPTION1","Markiere bei Benutzung")
+ZO_CreateStringId ("LTS_MARK_MAP_MENU_OPTION2","Markiere alle im Inventar")
+ZO_CreateStringId ("LTS_MARK_MAP_MENU_OPTION3","Markiere alle Standorte")
+
+ZO_CreateStringId ("LTS_MARKER_DELAY","BETA: Verzögerung zur Löschung der Markierung")
+ZO_CreateStringId ("LTS_MARKER_DELAY_TOOLTIP","Hier kannst du Verzögerung in Sekunden einstellen, bevor eine Markierung gelöscht wird. Diese Einstellung funktioniert nur, wenn du oben \'Markiere alle im Inventar\' ausgewählt hast.")
