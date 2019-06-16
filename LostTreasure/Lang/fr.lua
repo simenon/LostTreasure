376577e6a5d05deeab08b59a209d39a979fc5f76
Albert Simenon <albert@simenon.nl>
2019-06-16 19:45:35 +0200

    Updated french translation provided by lexo1000

--- LostTreasure/Lang/fr.lua
+++ LostTreasure/Lang/fr.lua
@@ -1,33 +1,33 @@
 ZO_CreateStringId ("LTS_TREASURE_ON_MAP","Afficher les trésors sur la carte")
-ZO_CreateStringId ("LTS_TREASURE_ON_MAP_TOOLTIP","Affiche la position des trésors sur la carte du monde.")
+ZO_CreateStringId ("LTS_TREASURE_ON_MAP_TOOLTIP","Affiche l'emplacement des trésors sur la carte.")
 ZO_CreateStringId ("LTS_TREASURE_ON_COMPASS","Afficher les trésors dans la boussole")
-ZO_CreateStringId ("LTS_TREASURE_ON_COMPASS_TOOLTIP","Affiche la position des trésors dans la boussole.")
+ZO_CreateStringId ("LTS_TREASURE_ON_COMPASS_TOOLTIP","Affiche l'emplacement des trésors dans la boussole.")
 ZO_CreateStringId ("LTS_TREASURE_ICON","Apparence des marqueurs de trésor")
-ZO_CreateStringId ("LTS_TREASURE_ICON_TOOLTIP","Détermine l'apparence des marqueurs de trésors.")
+ZO_CreateStringId ("LTS_TREASURE_ICON_TOOLTIP","Détermine l'apparence des marqueurs de trésor.")
 ZO_CreateStringId ("LTS_TREASURE_MARK_WHICH","Affichage des marqueurs de trésor")
-ZO_CreateStringId ("LTS_TREASURE_MARK_WHICH_TOOLTIP","Détermine si tous les marqueurs de trésors sont affichés sur la carte ou seuls ceux dont la carte est possédé.")
-
+ZO_CreateStringId ("LTS_TREASURE_MARK_WHICH_TOOLTIP", "Détermine les conditions d'affichage des marqueurs de trésor.")
+ 
 ZO_CreateStringId ("LTS_SURVEYS_ON_MAP","Afficher les repérages d'artisanat sur la carte")
-ZO_CreateStringId ("LTS_SURVEYS_ON_MAP_TOOLTIP","Affiche la position des repérages d'artisanat sur la carte du monde.")
+ZO_CreateStringId ("LTS_SURVEYS_ON_MAP_TOOLTIP","Affiche la position des repérages d'artisanat sur la carte.")
 ZO_CreateStringId ("LTS_SURVEYS_ON_COMPASS","Afficher les repérages d'artisanat dans la boussole")
 ZO_CreateStringId ("LTS_SURVEYS_ON_COMPASS_TOOLTIP","Affiche la position des repérages d'artisanat dans la boussole.")
-ZO_CreateStringId ("LTS_SURVEYS_ICON","Apparence des marqueurs de repérage")
+ZO_CreateStringId ("LTS_SURVEYS_ICON","Apparence des marqueurs d'artisanat")
 ZO_CreateStringId ("LTS_SURVEYS_ICON_TOOLTIP","Détermine l'apparence des marqueurs de repérage d'artisanat.")
-ZO_CreateStringId ("LTS_SURVEYS_MARK_WHICH","Affichage des marqueurs de repérage")
-ZO_CreateStringId ("LTS_SURVEYS_MARK_WHICH_TOOLTIP","Détermine si tous les marqueurs de repérage d'artisanat sont affichés sur la carte ou seuls ceux dont le repérage est possédé.")
-
+ZO_CreateStringId ("LTS_SURVEYS_MARK_WHICH","Affichage des marqueurs d'artisanat")
+ZO_CreateStringId ("LTS_SURVEYS_MARK_WHICH_TOOLTIP", "Détermine les conditions d'affichage des marqueurs de repérage d'artisanat.")
+ 
 ZO_CreateStringId ("LTS_PIN_SIZE","Taille des marqueurs")
 ZO_CreateStringId ("LTS_PIN_SIZE_TOOLTIP","Détermine la taille des marqueurs.")
-
+ 
 ZO_CreateStringId ("LTS_PIN_LEVEL","Niveau des marqueurs")
-ZO_CreateStringId ("LTS_PIN_LEVEL_TOOLTIP","Un niveau plus élevé positionne le marqueur au-dessus des autres. Augmenter cette valeur si le marqueur est caché par un autre.")
-
+ZO_CreateStringId ("LTS_PIN_LEVEL_TOOLTIP", "Un niveau supérieur positionne le marqueur au-dessus des autres. Augmenter cette valeur si le marqueur est caché par un autre.")
+ 
 ZO_CreateStringId ("LTS_SHOW_MINIMAP","Afficher la mini-carte au trésor")
 ZO_CreateStringId ("LTS_SHOW_MINIMAP_TOOLTIP","Affiche une mini-carte lorsque une carte est utilisée depuis l'inventaire.")
-
-ZO_CreateStringId ("LTS_MARK_MAP_MENU_OPTION1","Utilisé")
-ZO_CreateStringId ("LTS_MARK_MAP_MENU_OPTION2","Dans l'inventaire")
+ 
+ZO_CreateStringId ("LTS_MARK_MAP_MENU_OPTION1","Marquer à l'utilisation")
+ZO_CreateStringId ("LTS_MARK_MAP_MENU_OPTION2","Carte dans l'inventaire")
 ZO_CreateStringId ("LTS_MARK_MAP_MENU_OPTION3","Tous")
-
+ 
 ZO_CreateStringId ("LTS_MARKER_DELAY","BETA : Délai d'effacement des marqueurs")
-ZO_CreateStringId ("LTS_MARKER_DELAY_TOOLTIP","Ajoute un délai avant d'effacer un emplacement marqué sur la carte. Fonctionne seulement en conjonction avec 'Utilisé' et 'Dans l'inventaire'. Cela ne fonctionne pas lors de l'ouverture de la carte car tous les marqueurs sont actualisés.")
+ZO_CreateStringId ("LTS_MARKER_DELAY_TOOLTIP", "Ajoute un délai avant d'effacer un emplacement marqué sur la carte. Fonctionne seulement en conjonction avec les options 'Marquer à l'utilisation' et 'Dans l'inventaire'.\nNe fonctionne pas lors de l'ouverture de la carte car tous les marqueurs sont actualisés."
